91fcf3995973281add77e90b173ecec746c51684
Maxi <93052@sistemas.frc.utn.edu.ar>
2025-11-25 22:48:26 -0300

    tabla clientes

--- main.py
+++ main.py
@@ -1,16 +1,7 @@
-from controlladores.controller_contrato import ContratoController
-from services.containers.container import Container
 from ui.app import run_app
 
 def main():
-    containter = Container()
-    controller = containter.reporte_controller()
-    print(controller.get_detalle_flota())
-    print(controller.get_facturacion_mensual())
-    print(controller.get_clientes_contacto())
-    print(controller.get_utilizacion_flota())
-    print(controller.get_disponibilidad_flota())
-    print(controller.get_rentabilidad_contratos())
+    run_app()
 
 
 if __name__ == "__main__":
